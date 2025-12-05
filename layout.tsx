9fa88ce19e3cdee93eeeb4c7ada94eb91a6d4604
chungyan8668-hash <chungyan8668@gmail.com>
2025-12-05 11:33:41 +0800

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen bg-white dark:bg-black text-black dark:text-white">
+        {children}
+      </body>
+    </html>
+  );
+}
